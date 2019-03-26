ef579fb665a8998d6c3013132f0a75382e72f6dc
Elekoot <kootoomo@gmail.com>
2019-03-26 22:46:50 +0300

    error handling, ok, last commit

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -11,4 +11,15 @@ def home(request):
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
     return render(request, 'home.html', {'api': api, 'price': price})
-    
+
+def prices(request):
+    if request.method == 'POST':
+        quote = request.POST['quote']
+        quote = quote.upper()
+        crypto_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" + quote + "&tsyms=USD")
+        crypto = json.loads(crypto_request.content)
+        return render(request, 'prices.html', {'quote':quote, 'crypto': crypto})
+
+    else:
+        notfound = "Enter a crypto currency symbol into the form above..."
+        return render(request, 'prices.html', {'notfound': notfound})
