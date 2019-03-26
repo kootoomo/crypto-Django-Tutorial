19ec1072acf322d58a7bf69d51326af3c5bbe950
Elekoot <kootoomo@gmail.com>
2019-03-26 19:40:22 +0300

    Display items of BTC and XRP

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -3,7 +3,12 @@ import requests
 import json
 
 def home(request):
+    # Grab Crypto Price Data
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP&tsyms=USD")
+    price = json.loads(price_request.content)
+
+    # Grab Crypto News
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
-    return render(request, 'home.html', {'api': api})
+    return render(request, 'home.html', {'api': api, 'price': price})
     
